cf1e22adcd4ee69bf41611593f5fff7036298a5d
Carlos Alvarez <mathdebian@debian>
2015-01-20 14:24:38 -0600

    Agregados los servicios. Tipo de denuncias.

--- app/Controller/ServicesController.php
+++ app/Controller/ServicesController.php
@@ -28,7 +28,28 @@ class ServicesController extends AppController {
 		return json_encode($datos);
 	}
 
-	public function pruebas() {
-		
+		/**
+	 *
+	 */
+	public function tipoDenuncias() {
+		$this->autoRender = false;
+		//$this->request->onlyAllow('ajax');
+		$this->response->type('json');
+
+		$datos = array(
+			'delation_infos' => array(
+				array('id' => 1, 'kind' => 'Empleado perdiendo el tiempo'),
+				array('id' => 2, 'kind' => 'Acoso laboral'),
+				array('id' => 3, 'kind' => 'Acoso sexual'),
+				array('id' => 4, 'kind' => 'Abandono de trabajo'),
+				array('id' => 5, 'kind' => 'Uso inadecuado de la propiedad pública'),
+				array('id' => 6, 'kind' => 'Soborno'),
+				array('id' => 7, 'kind' => 'Negligencia'),
+			)
+		);
+
+		return json_encode($datos);
 	}
+
+	//public function pruebas() {}
 }
